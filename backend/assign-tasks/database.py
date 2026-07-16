5b2679d21f277bfe101e90466fbe1dadf6b698a5
Maham Maryam <152018095+Maham38@users.noreply.github.com>
2026-07-16 03:00:19 -0700

    Change database
    Updated database

--- backend/assign-tasks/database.py
+++ backend/assign-tasks/database.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-engine = create_engine(os.getenv("DATABASE_URL", "postgresql+psycopg://neondb_owner:npg_G9z0yAghNXbR@ep-noisy-term-at58ctry.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require"), pool_pre_ping=True)
+engine = create_engine(os.getenv("DATABASE_URL", ""), pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 class Base(DeclarativeBase): pass
 def get_db():
